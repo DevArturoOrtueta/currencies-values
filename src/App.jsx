a801a2433bd363a004766bfe3a7c7d280f61eeae
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-10-06 16:53:00 -0500

    Update getFrom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
-import { getFrom } from './hooks/getFrom'
+
 
 function App() {
-  console.log(getFrom('https://api.frankfurter.app/latest?from=USD'))
+  
 }
 
 export default App
--- src/hooks/getFrom.js
+++ src/hooks/getFrom.js
@@ -1,6 +1,28 @@
 
 export const getFrom = async (url) => {
-    const response = await fetch(url);
-    const datos = await response.json()
-    console.log(datos)
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then((response) => {
+                if(response.ok){
+                    return response.json();
+                }
+                reject(
+                    "No hemos podido recuperar ese json. El código de respuesta del servidor es: " +
+                      response.status
+                );
+            })
+            .then((json) => resolve(json))
+            .catch((err) => reject(err));
+    })
 }
+
+
+//Uso
+
+// getFrom('https://api.frankfurter.app/latest?from=USD')
+//     .then((json)=> {
+//       //Conseguir la data de la variable json
+//     })
+//     .catch((err)=> {
+//       //Conseguir el error de la variable err
+//     })
