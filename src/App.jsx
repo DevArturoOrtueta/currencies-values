69bbb047cea7f4d57c89a2b295b0c0f80d4dbde2
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-10-09 09:44:16 -0500

    Add return to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import {Button} from "@nextui-org/react";
 
 function App() {
-    <Button color="primary">
-    Button
-  </Button>
+    return (
+      <Button color="primary">
+        Button
+      </Button>
+    )
 }
 
 export default App
