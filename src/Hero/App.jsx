3146feda885abb11057d0341a3328d06b56c4642
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-10-09 21:57:51 -0500

    Update Hero section

---
+++ src/Hero/App.jsx
@@ -0,0 +1,16 @@
+import './css/style.css';
+
+function App() {
+
+  return (
+    <div className="hero">
+      <div className='hero-content'>
+        <p>Every most used currencies in one site...</p>
+        <span>Powered by api.frankfurter.app</span>
+      </div>
+    </div>
+
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { NextUIProvider } from '@nextui-org/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-
+import App from './Hero/App'
+import './general.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
